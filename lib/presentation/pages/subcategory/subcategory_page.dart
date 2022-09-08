7b4c6fc0b31c628da16629fe77277182854485ef
jan cahlik <honzicekcahlicek1@gmail.com>
2022-09-08 18:43:52 +0200

    container 🎟

--- lib/presentation/pages/subcategory/subcategory_page.dart
+++ lib/presentation/pages/subcategory/subcategory_page.dart
@@ -4,6 +4,7 @@ import 'package:getwidget/components/loader/gf_loader.dart';
 import 'package:medic_pro_bloc/domain/subject/questionsection.dart';
 import 'package:medic_pro_bloc/domain/subject/subcategory.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:sizer/sizer.dart';
 
 import '../../../domain/subject/article.dart';
 import '../../core/app_bar.dart';
@@ -24,66 +25,27 @@ class SubcategoryPage extends StatelessWidget {
           vertical: UIConstants.safeAreaPaddingVertical,
           horizontal: UIConstants.safeAreaPaddingHorizontal
         ),
-        child: Column(
-          children: [
-            Text(currentSubcategory.title).tr(),
-            StreamBuilder(
-                stream: currentSubcategory.articles,
-                builder: (BuildContext context, AsyncSnapshot snapshot){
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const GFLoader();
-                  }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done){
+        child: Container(
+          // todo fix ui
+          height: 50.h,
+          child: Column(
+            children: [
+              Text(currentSubcategory.title).tr(),
+              StreamBuilder(
+                  stream: currentSubcategory.articles,
+                  builder: (BuildContext context, AsyncSnapshot snapshot){
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const GFLoader();
+                    }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done){
 
-                    return Card(
-                      child: Column(
-                        children: [
-                          ListView.builder(
-                            itemCount: snapshot.data.length,
-                            shrinkWrap: true,
-                            itemBuilder: (context, categoryIndex){
-                            final Article currentArticle = snapshot.data[categoryIndex];
-                              return Card(
-                                  child: ListTile(
-                                    // leading: SizedBox(
-                                    //   height: 20,
-                                    //   width: 20,
-                                    //   child: Image.asset('assets/${subjectSubcategory.title}.png'),
-                                    // ),
-                                    title: Text(currentArticle.titleUI),
-  
-                                    onTap: (){
-                                      // // on widget tap set the current subject subcategory to provider
-                                      // context.read<QuestionSectionProvider>().setCurrentSubjectSubcategory(subjectSubcategory);
-                                      //
-                                      // Navigator.push(context, PageRouteBuilder(pageBuilder: (_, __, ___) => const SubjectSubcategoryScreen()));
-                                    },
-                                  )
-                              );
-                            }
-                          )
-                        ],
-                      )
-                    );
-                  }
-                  return const GFLoader();
-                }
-            ),
-            const Spacer(),
-            StreamBuilder(
-                stream: currentSubcategory.questionSections,
-                builder: (BuildContext context, AsyncSnapshot snapshot){
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const GFLoader();
-                  }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done){
-
-                    return Card(
-                      child: Column(
-                        children: [
-                          ListView.builder(
+                      return Card(
+                        child: Column(
+                          children: [
+                            ListView.builder(
                               itemCount: snapshot.data.length,
                               shrinkWrap: true,
                               itemBuilder: (context, categoryIndex){
-                                final QuestionSection currentQuestionSection = snapshot.data[categoryIndex];
+                              final Article currentArticle = snapshot.data[categoryIndex];
                                 return Card(
                                     child: ListTile(
                                       // leading: SizedBox(
@@ -91,8 +53,8 @@ class SubcategoryPage extends StatelessWidget {
                                       //   width: 20,
                                       //   child: Image.asset('assets/${subjectSubcategory.title}.png'),
                                       // ),
-                                      title: Text(currentQuestionSection.titleUI),
-
+                                      title: Text(currentArticle.titleUI),
+  
                                       onTap: (){
                                         // // on widget tap set the current subject subcategory to provider
                                         // context.read<QuestionSectionProvider>().setCurrentSubjectSubcategory(subjectSubcategory);
@@ -102,15 +64,58 @@ class SubcategoryPage extends StatelessWidget {
                                     )
                                 );
                               }
-                          ),
-                        ],
-                      ),
-                    );
+                            )
+                          ],
+                        )
+                      );
+                    }
+                    return const GFLoader();
                   }
-                  return const GFLoader();
-                }
-            ),
-          ],
+              ),
+              const Spacer(),
+              StreamBuilder(
+                  stream: currentSubcategory.questionSections,
+                  builder: (BuildContext context, AsyncSnapshot snapshot){
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const GFLoader();
+                    }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done){
+
+                      return Card(
+                        child: Column(
+                          children: [
+                            ListView.builder(
+                                itemCount: snapshot.data.length,
+                                shrinkWrap: true,
+                                itemBuilder: (context, categoryIndex){
+                                  final QuestionSection currentQuestionSection = snapshot.data[categoryIndex];
+                                  return Card(
+                                      child: ListTile(
+                                        // leading: SizedBox(
+                                        //   height: 20,
+                                        //   width: 20,
+                                        //   child: Image.asset('assets/${subjectSubcategory.title}.png'),
+                                        // ),
+                                        title: Text(currentQuestionSection.titleUI),
+
+                                        onTap: (){
+                                          // // on widget tap set the current subject subcategory to provider
+                                          // context.read<QuestionSectionProvider>().setCurrentSubjectSubcategory(subjectSubcategory);
+                                          //
+                                          // Navigator.push(context, PageRouteBuilder(pageBuilder: (_, __, ___) => const SubjectSubcategoryScreen()));
+                                        },
+                                      )
+                                  );
+                                }
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                    return const GFLoader();
+                  }
+              ),
+            ],
+          ),
         )
       )
     );
