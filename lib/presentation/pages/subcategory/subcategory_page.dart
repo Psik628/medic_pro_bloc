3bffaa1d976874fe0c472ce81d6316224479fc5a
jan cahlik <honzicekcahlicek1@gmail.com>
2022-09-08 18:13:47 +0200

    rendering articles and question sections to subcategory page 🎃

--- lib/presentation/pages/subcategory/subcategory_page.dart
+++ lib/presentation/pages/subcategory/subcategory_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:getwidget/components/loader/gf_loader.dart';
+import 'package:medic_pro_bloc/domain/subject/questionsection.dart';
 import 'package:medic_pro_bloc/domain/subject/subcategory.dart';
 
+import '../../../domain/subject/article.dart';
 import '../../core/app_bar.dart';
 import '../../ui_constants.dart';
 
@@ -20,7 +23,49 @@ class SubcategoryPage extends StatelessWidget {
           vertical: UIConstants.safeAreaPaddingVertical,
           horizontal: UIConstants.safeAreaPaddingHorizontal
         ),
-        child: const Text('page')
+        child: Column(
+          children: [
+            StreamBuilder(
+                stream: currentSubcategory.articles,
+                builder: (BuildContext context, AsyncSnapshot snapshot){
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const GFLoader();
+                  }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done){
+
+                    return ListView.builder(
+                        itemCount: snapshot.data.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, categoryIndex){
+                          final Article currentArticle = snapshot.data[categoryIndex];
+                          return Text(currentArticle.title);
+                        }
+                    );
+                  }
+                  return const GFLoader();
+                }
+            ),
+            const Spacer(),
+            StreamBuilder(
+                stream: currentSubcategory.questionSections,
+                builder: (BuildContext context, AsyncSnapshot snapshot){
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const GFLoader();
+                  }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done){
+
+                    return ListView.builder(
+                        itemCount: snapshot.data.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, categoryIndex){
+                          final QuestionSection currentQuestionSection = snapshot.data[categoryIndex];
+                          return Text(currentQuestionSection.title);
+                        }
+                    );
+                  }
+                  return const GFLoader();
+                }
+            ),
+          ],
+        )
       )
     );
   }
