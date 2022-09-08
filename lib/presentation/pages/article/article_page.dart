9589b35e94cc0d0f061b51d207687cd9ecabd20a
jan cahlik <honzicekcahlicek1@gmail.com>
2022-09-08 20:07:03 +0200

    alignment fix 🎏

--- lib/presentation/pages/article/article_page.dart
+++ lib/presentation/pages/article/article_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -48,10 +49,106 @@ class ArticlePage extends StatelessWidget {
         child: Column(
           children: [
             Center(
-              child: Text(
-                  currentArticle.title,
-              ),
+              child: Text(currentArticle.title).tr(),
             ),
+            ...currentArticle.content.map((snippet){
+              
+              String snippetString = snippet[ArticleConstants.content].toString();
+
+              if(snippetString.startsWith(ArticleConstants.heading1Start) || snippetString.startsWith(ArticleConstants.heading2Start) || snippetString.startsWith(ArticleConstants.heading3Start)){
+                return Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                      snippetString.substring(3),
+                      style: TextStyle(
+                          fontWeight: snippetString.startsWith(ArticleConstants.heading1Start) ? UIConstants.articlePageH1FontWeight : snippetString.startsWith(ArticleConstants.heading2Start) ? UIConstants.articlePageH2FontWeight : UIConstants.articlePageH3FontWeight,
+                          fontSize: snippetString.startsWith(ArticleConstants.heading1Start) ? UIConstants.articlePageH1FontSize : snippetString.startsWith(ArticleConstants.heading2Start) ?  UIConstants.articlePageH1FontSize : UIConstants.articlePageH3FontSize
+                      )
+                  ),
+                );
+              }else if(snippetString.contains(ArticleConstants.boldStarter)){
+                // is bold text
+                String snippetContent = snippet[ArticleConstants.content].toString();
+                // splitting to every word
+                List<String> contentParts = snippetContent.split(' ');
+
+                return Align(
+                  alignment: Alignment.centerLeft,
+                  child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        fontSize: UIConstants.articlePageNormalTextFontSize,
+                      ),
+                      children: <TextSpan>[
+                        ...contentParts.map((content) {
+                          return TextSpan(
+                              text: "${content.replaceAll(ArticleConstants.boldStarter, '')} ",
+                              style: TextStyle(fontWeight: (content.toString().startsWith(ArticleConstants.boldStarter) || content.toString().endsWith(ArticleConstants.boldStarter)) ? FontWeight.bold : FontWeight.normal)
+                          );
+                        })
+                      ],
+                    ),
+                  ),
+                );
+              }else if(snippetString.startsWith(ArticleConstants.imageStart)){
+
+                // todo implement this
+                // return FutureBuilder(
+                //     future: FirebaseStorage.instance.ref(snippet[ArticleConstants.content].toString().replaceFirst(ArticleConstants.imageStart,'')).getDownloadURL(),
+                //     builder: ((BuildContext context, AsyncSnapshot<String> snapshot){
+                //       if(!snapshot.hasData) {
+                //         return Container(
+                //             width: MediaQuery.of(context).size.width,
+                //             height: defaultImageHeight,
+                //             child: const GFLoader()
+                //         );
+                //       }
+                //       if(snapshot.hasError){
+                //         if (kDebugMode) {
+                //           print(snapshot.error);
+                //         }
+                //         return Text(SOMETHING_WENT_WRONG);
+                //       }
+                //
+                //       return Container(
+                //         width: MediaQuery.of(context).size.width,
+                //         height: defaultImageHeight,
+                //         child: Image.network(
+                //             snapshot.data!,
+                //             fit: BoxFit.contain
+                //         ),
+                //       );
+                //     })
+                // );
+                return const Text("placeholder image");
+              } else if(snippetString.startsWith(ArticleConstants.listStart)){
+                return Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    ArticleConstants.listSeparator + snippetString.replaceFirst(ArticleConstants.listStart,'')
+                  ),
+                );
+              } else{
+                // is normal text
+                return
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontSize: UIConstants.articlePageNormalTextFontSize,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: snippet[ArticleConstants.content],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+              }
+              }
+            )
           ],
         )
       )
--- lib/presentation/ui_constants.dart
+++ lib/presentation/ui_constants.dart
@@ -1,4 +1,16 @@
+import 'dart:ui';
+
 abstract class UIConstants{
+  // todo make responsive with sizer
   static double safeAreaPaddingHorizontal = 20;
   static double safeAreaPaddingVertical = 20;
+
+  // ARTICLE PAGE
+  static double articlePageH1FontSize = 28;
+  static double articlePageH2FontSize = 25;
+  static double articlePageH3FontSize = 20;
+  static FontWeight articlePageH1FontWeight = FontWeight.bold;
+  static FontWeight articlePageH2FontWeight = FontWeight.w500;
+  static FontWeight articlePageH3FontWeight = FontWeight.normal;
+  static double articlePageNormalTextFontSize = 14;
 }
