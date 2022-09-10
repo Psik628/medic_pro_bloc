60ea3de349497c5bcacf0a6cde9e3737149f7fb5
jan cahlik <honzicekcahlicek1@gmail.com>
2022-09-10 20:38:50 +0200

    added event handlers 🧤

--- lib/application/question_section/question_section_bloc.dart
+++ lib/application/question_section/question_section_bloc.dart
@@ -3,7 +3,8 @@ import 'package:equatable/equatable.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:medic_pro_bloc/domain/subject/question.dart';
-import 'package:meta/meta.dart';
+
+import '../../domain/subject/option.dart';
 
 part 'question_section_event.dart';
 part 'question_section_state.dart';
@@ -12,12 +13,17 @@ part 'question_section_bloc.freezed.dart';
 
 @injectable
 class QuestionSectionBloc extends Bloc<QuestionSectionEvent, QuestionSectionState> {
-
-
-
-
   QuestionSectionBloc() : super(QuestionSectionState.initial()) {
-    on<QuestionSectionEvent>((event, emit) {
+    on<AnswerQuestion>((AnswerQuestion event, emit) {
+      // TODO: implement event handler
+    });
+    on<AnswerFinalQuestion>((AnswerFinalQuestion event, emit) {
+      // TODO: implement event handler
+    });
+    on<SelectOption>((SelectOption event, emit) {
+      // TODO: implement event handler
+    });
+    on<UnSelectOption>((UnSelectOption event, emit) {
       // TODO: implement event handler
     });
   }
--- lib/application/question_section/question_section_event.dart
+++ lib/application/question_section/question_section_event.dart
@@ -1,4 +1,9 @@
 part of 'question_section_bloc.dart';
 
-@immutable
-abstract class QuestionSectionEvent {}
+@freezed
+class QuestionSectionEvent with _$QuestionSectionEvent{
+  const factory QuestionSectionEvent.answerQuestion({required Question question}) = AnswerQuestion;
+  const factory QuestionSectionEvent.answerFinalQuestion({required Question question}) = AnswerFinalQuestion;
+  const factory QuestionSectionEvent.selectOption({required Question question, required Option option}) = SelectOption;
+  const factory QuestionSectionEvent.unSelectOption({required Question question, required Option option}) = UnSelectOption;
+}
