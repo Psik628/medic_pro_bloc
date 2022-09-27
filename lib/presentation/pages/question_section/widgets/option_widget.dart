b56c057de8ec324398f1f4a6f6fb75428e2f9ba9
jan cahlik <honzicekcahlicek1@gmail.com>
2022-09-27 23:36:32 +0200

    button mods when selected 🎃

--- lib/presentation/pages/question_section/widgets/option_widget.dart
+++ lib/presentation/pages/question_section/widgets/option_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:getwidget/components/button/gf_button.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:medic_pro_bloc/application/question_section/question_section_bloc.dart';
 
 import '../../../../domain/subject/option.dart';
@@ -23,30 +24,11 @@ class _OptionWidgetState extends State<OptionWidget> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        // bloc is provided in MultiBlocProvider in app_widget.dart
-        // BlocBuilder<QuestionSectionBloc, QuestionSectionState>(
-        //   builder: (context, state) {
-        //     return Expanded(
-        //         child: GFButton(
-        //           fullWidthButton: true,
-        //           onPressed: () {
-        //             setState(() {
-        //               isSelected = !isSelected;
-        //             });
-        //             if(isSelected){
-        //               context.read<QuestionSectionBloc>().add(QuestionSectionEvent.selectOption(option: widget.currentOption));
-        //             }else{
-        //               context.read<QuestionSectionBloc>().add(QuestionSectionEvent.unSelectOption(option: widget.currentOption));
-        //             }
-        //           },
-        //           child: Text(widget.currentOption.content),
-        //         )
-        //     );
-        //   },
-        // ),
         Expanded(
           child: GFButton(
-          fullWidthButton: true,
+            fullWidthButton: true,
+            shape: isSelected ? GFButtonShape.pills : GFButtonShape.square,
+            color: isSelected ? GFColors.SUCCESS : GFColors.INFO,
             onPressed: () {
               setState(() {
                 isSelected = !isSelected;
