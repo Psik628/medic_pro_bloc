c265f8f4c4144b8fb50b5c571c4696a855dcd813
jan cahlik <honzicekcahlicek1@gmail.com>
2022-09-28 00:31:45 +0200

    has size error 🤞

--- lib/presentation/pages/question_section/widgets/results_page_widget.dart
+++ lib/presentation/pages/question_section/widgets/results_page_widget.dart
@@ -4,8 +4,10 @@ import 'package:getwidget/colors/gf_color.dart';
 import 'package:getwidget/components/button/gf_button.dart';
 import 'package:getwidget/components/button/gf_button_bar.dart';
 import 'package:getwidget/components/card/gf_card.dart';
-import 'package:getwidget/components/list_tile/gf_list_tile.dart';
+import 'package:getwidget/components/loader/gf_loader.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:medic_pro_bloc/application/question_section/question_section_bloc.dart';
+import 'package:sizer/sizer.dart';
 
 class ResultsPageWidget extends StatelessWidget {
   const ResultsPageWidget({Key? key}) : super(key: key);
@@ -18,83 +20,62 @@ class ResultsPageWidget extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                GFCard(
-                  color: GFColors.DANGER,
-                  boxFit: BoxFit.cover,
-                  content: Text("Some quick example text to build on the card"),
-                  buttonBar: GFButtonBar(
-                    direction: Axis.vertical,
-                    children: <Widget>[
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Some Answer',
-                      ),
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Cancel',
-                      ),
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Buy',
-                      ),
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Buy',
-                      ),
-                    ],
-                  ),
-                ),
-                GFCard(
-                  color: GFColors.SUCCESS,
-                  boxFit: BoxFit.cover,
-                  content: Text("Some quick example text to build on the card"),
-                  buttonBar: GFButtonBar(
-                    direction: Axis.vertical,
-                    children: <Widget>[
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Some Answer',
-                      ),
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Cancel',
-                      ),
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Buy',
-                      ),
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Buy',
-                      ),
-                    ],
-                  ),
-                ),
-                GFCard(
-                  color: GFColors.WARNING,
-                  boxFit: BoxFit.cover,
-                  content: Text("Some quick example text to build on the card"),
-                  buttonBar: GFButtonBar(
-                    direction: Axis.vertical,
-                    children: <Widget>[
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Some Answer',
-                      ),
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Cancel',
-                      ),
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Buy',
-                      ),
-                      GFButton(
-                        onPressed: () {},
-                        text: 'Buy',
-                      ),
-                    ],
-                  ),
+                BlocBuilder<QuestionSectionBloc, QuestionSectionState>(
+                  builder: (context, state) {
+
+                    return ListView.builder(
+                      itemCount: state.questions.length,
+                      shrinkWrap: true,
+                      // this makes this SingleChildScrollView scrollable
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, questionIndex) {
+                        return
+                          GFCard(
+                            color: GFColors.DANGER,
+                            boxFit: BoxFit.cover,
+                            content: Text(
+                              // todo solve null
+                              state.questions[questionIndex].content!
+                            ),
+                            buttonBar: GFButtonBar(
+                              direction: Axis.vertical,
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 100,
+                                  child: StreamBuilder(
+                                    stream: state.questions[questionIndex].options,
+                                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+
+                                    if (snapshot.connectionState == ConnectionState.waiting) {
+                                      return const GFLoader(type: GFLoaderType.square,);
+                                    }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done) {
+                                      return Expanded(
+                                        child: ListView.builder(
+                                            itemCount: snapshot.data.length,
+                                            shrinkWrap: true,
+                                            itemBuilder: (context, optionIndex) {
+                                              return SizedBox(
+                                                height: 100,
+                                                child: GFButton(
+                                                  onPressed: () {},
+                                                  text: snapshot.data.options[optionIndex].content,
+                                                ),
+                                              );
+                                            }
+                                        ),
+                                      );
+                                    } else{
+                                      return const GFLoader(type: GFLoaderType.ios,);
+                                    }
+                                  }
+                              ),
+                                )
+                            ],
+                          ),
+                        );
+                      }
+                    );
+                  },
                 ),
               ],
             ),
