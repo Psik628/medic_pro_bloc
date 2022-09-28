2fdf54b0672846979daed86ba4cd66a77432e99f
jan cahlik <honzicekcahlicek1@gmail.com>
2022-09-28 14:46:52 +0200

    do some other day 😉

--- lib/presentation/pages/question_section/widgets/results_page_card_widget.dart
+++ lib/presentation/pages/question_section/widgets/results_page_card_widget.dart
@@ -27,60 +27,64 @@ class _ResultsPageCardWidgetState extends State<ResultsPageCardWidget> {
 
   List<Question> answeredCorrectlyQuestions = [];
 
+  // int evaluator(){
+  //
+  // }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<QuestionSectionBloc, QuestionSectionState>(
       builder: (context, state) {
-        return GFCard(
-          // color: state.questions[widget.questionIndex].answeredCorrectly() == true && state.questions[widget.questionIndex].correctQuestionsLength == state.questions[widget.questionIndex].selectedOptions.length ? GFColors.SUCCESS : GFColors.DANGER,
-          content: Text(state.questions[widget.questionIndex].content!),
-          buttonBar: GFButtonBar(
-            children: [
-              StreamBuilder(
-                  stream: state.questions[widget.questionIndex].options,
-                  builder: (BuildContext context, AsyncSnapshot<List<Option>> snapshot) {
+        return StreamBuilder(
+          stream: state.questions[widget.questionIndex].options,
+          builder: (BuildContext context, AsyncSnapshot<List<Option>> snapshot) {
 
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const GFLoader(type: GFLoaderType.square);
-                    }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done) {
-                      // // notify
-                      // context.read<QuestionSectionBloc>().add(QuestionSectionEvent.setCorrectOptionsLength(questionToDisplayIndex: widget.questionIndex,correctOptionsLength: snapshot.data!.length));
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const GFLoader(type: GFLoaderType.square);
+            }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done) {
+              // // notify
+              // context.read<QuestionSectionBloc>().add(QuestionSectionEvent.setCorrectOptionsLength(questionToDisplayIndex: widget.questionIndex,correctOptionsLength: snapshot.data!.length));
 
-                      // print(state.questions[widget.questionIndex].content);
-                      //
-                      // int correctOptionsLength = snapshot.data!.where((option) => option.correct).length;
+              // print(state.questions[widget.questionIndex].content);
+              //
+              // int correctOptionsLength = snapshot.data!.where((option) => option.correct).length;
 
-                      // // all options that were chosen are correct and all correct options were chosen
-                      // if(state.questions[widget.questionIndex].answeredCorrectly() && state.questions[widget.questionIndex].selectedOptions.length == correctOptionsLength){
-                      //   setState(() {
-                      //     answeredCorrectlyQuestions: [...answeredCorrectlyQuestions.add(state.questions[widget.questionIndex])];
-                      //   });
-                      //   print('Correctly answered questions length');
-                      //   print(answeredCorrectlyQuestions.length);
-                      // }
+              // // all options that were chosen are correct and all correct options were chosen
+              // if(state.questions[widget.questionIndex].answeredCorrectly() && state.questions[widget.questionIndex].selectedOptions.length == correctOptionsLength){
+              //   setState(() {
+              //     answeredCorrectlyQuestions: [...answeredCorrectlyQuestions.add(state.questions[widget.questionIndex])];
+              //   });
+              //   print('Correctly answered questions length');
+              //   print(answeredCorrectlyQuestions.length);
+              // }
 
-                      return ListView.builder(
-                          itemCount: snapshot.data!.length,
-                          shrinkWrap: true,
-                          // making the options list view not scrollable
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemBuilder: (context, optionIndex) {
-                            return GFButton(
-                              onPressed: () {},
-                              color: snapshot.data![optionIndex].correct ? GFColors.SUCCESS : GFColors.DANGER,
-                              // checking if the current option is in QuestionSectionState selectedOptions field
-                              shape: state.questions[widget.questionIndex].selectedOptions.contains(snapshot.data![optionIndex]) ? GFButtonShape.pills : GFButtonShape.square,
-                              text: snapshot.data![optionIndex].content,
-                            );
-                          }
-                      );
-                    } else{
-                      return const GFLoader(type: GFLoaderType.ios);
-                    }
-                  }
-              ),
-            ],
-          ),
+              return GFCard(
+                    // color: state.questions[widget.questionIndex].answeredCorrectly() == true && state.questions[widget.questionIndex].correctQuestionsLength == state.questions[widget.questionIndex].selectedOptions.length ? GFColors.SUCCESS : GFColors.DANGER,
+                    content: Text(state.questions[widget.questionIndex].content!),
+                    buttonBar: GFButtonBar(
+                      children: [
+                        ListView.builder(
+                        itemCount: snapshot.data!.length,
+                            shrinkWrap: true,
+                            // making the options list view not scrollable
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemBuilder: (context, optionIndex) {
+                              return GFButton(
+                                onPressed: () {},
+                                color: snapshot.data![optionIndex].correct ? GFColors.SUCCESS : GFColors.DANGER,
+                                // checking if the current option is in QuestionSectionState selectedOptions field
+                                shape: state.questions[widget.questionIndex].selectedOptions.contains(snapshot.data![optionIndex]) ? GFButtonShape.pills : GFButtonShape.square,
+                                text: snapshot.data![optionIndex].content,
+                              );
+                            }
+                        )
+                      ]
+                  )
+              );
+            } else{
+              return const GFLoader(type: GFLoaderType.ios);
+            }
+          }
         );
       },
     );
