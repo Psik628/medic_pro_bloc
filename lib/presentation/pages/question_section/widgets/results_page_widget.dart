e71c271dd6f11b2e8126701951979dd1e8e8c714
jan cahlik <honzicekcahlicek1@gmail.com>
2022-09-28 11:18:25 +0200

    displaying answered questions correctly 👌

--- lib/presentation/pages/question_section/widgets/results_page_widget.dart
+++ lib/presentation/pages/question_section/widgets/results_page_widget.dart
@@ -14,74 +14,103 @@ class ResultsPageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<QuestionSectionBloc, QuestionSectionState>(
-      builder: (context, state) {
-        return Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                BlocBuilder<QuestionSectionBloc, QuestionSectionState>(
-                  builder: (context, state) {
-
-                    return ListView.builder(
-                      itemCount: state.questions.length,
-                      shrinkWrap: true,
-                      // this makes this SingleChildScrollView scrollable
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, questionIndex) {
-                        return
-                          GFCard(
-                            color: GFColors.DANGER,
-                            boxFit: BoxFit.cover,
-                            content: Text(
-                              // todo solve null
-                              state.questions[questionIndex].content!
-                            ),
-                            buttonBar: GFButtonBar(
-                              direction: Axis.vertical,
-                              children: <Widget>[
-                                SizedBox(
-                                  height: 100,
-                                  child: StreamBuilder(
-                                    stream: state.questions[questionIndex].options,
-                                    builder: (BuildContext context, AsyncSnapshot snapshot) {
-
-                                    if (snapshot.connectionState == ConnectionState.waiting) {
-                                      return const GFLoader(type: GFLoaderType.square,);
-                                    }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done) {
-                                      return Expanded(
-                                        child: ListView.builder(
-                                            itemCount: snapshot.data.length,
-                                            shrinkWrap: true,
-                                            itemBuilder: (context, optionIndex) {
-                                              return SizedBox(
-                                                height: 100,
-                                                child: GFButton(
-                                                  onPressed: () {},
-                                                  text: snapshot.data.options[optionIndex].content,
-                                                ),
-                                              );
-                                            }
-                                        ),
-                                      );
-                                    } else{
-                                      return const GFLoader(type: GFLoaderType.ios,);
-                                    }
-                                  }
-                              ),
-                                )
-                            ],
-                          ),
-                        );
-                      }
-                    );
-                  },
+    return Expanded(
+      child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80.h,
+                child: BlocBuilder<QuestionSectionBloc, QuestionSectionState>(
+                  builder: (context, QuestionSectionState state){
+                   return ListView.builder(
+                     itemCount: state.questions.length,
+                     itemBuilder: (context, questionIndex){
+                       return GFCard(
+                         content: Text(state.questions[questionIndex].content!),
+                       );
+                     }
+                   );
+                  }
                 ),
-              ],
-            ),
+              )
+            ],
           ),
-        );
-      },
+        ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //       return Expanded(
+  //         child: SingleChildScrollView(
+  //           child: Column(
+  //             children: [
+  //               BlocBuilder<QuestionSectionBloc, QuestionSectionState>(
+  //                 builder: (context, state) {
+  //
+  //                   return Flexible(
+  //                     child: ListView.builder(
+  //                       itemCount: state.questions.length,
+  //                       // shrinkWrap: true,
+  //                       // this makes this SingleChildScrollView scrollable
+  //                       // physics: const NeverScrollableScrollPhysics(),
+  //                       itemBuilder: (context, questionIndex) {
+  //                         return
+  //                           SizedBox(
+  //                             height: 300,
+  //                             child: GFCard(
+  //                               color: GFColors.DANGER,
+  //                               boxFit: BoxFit.cover,
+  //                               content: Text(
+  //                                 // todo solve null
+  //                                 state.questions[questionIndex].content!
+  //                               ),
+  //                               buttonBar: GFButtonBar(
+  //                                 direction: Axis.vertical,
+  //                                 children: <Widget>[
+  //                                   Flexible(
+  //                                     child: StreamBuilder(
+  //                                       stream: state.questions[questionIndex].options,
+  //                                       builder: (BuildContext context, AsyncSnapshot snapshot) {
+  //
+  //                                         if (snapshot.connectionState == ConnectionState.waiting) {
+  //                                           return const GFLoader(type: GFLoaderType.square);
+  //                                         }else if (snapshot.connectionState == ConnectionState.active || snapshot.connectionState == ConnectionState.done) {
+  //                                           return Flexible(
+  //                                             child: ListView.builder(
+  //                                                 itemCount: snapshot.data.length,
+  //                                                 shrinkWrap: true,
+  //                                                 itemBuilder: (context, optionIndex) {
+  //                                                   return SizedBox(
+  //                                                     height: 30,
+  //                                                     child: GFButton(
+  //                                                       onPressed: () {},
+  //                                                       text: snapshot.data.options[optionIndex].content,
+  //                                                     ),
+  //                                                   );
+  //                                                 }
+  //                                             ),
+  //                                           );
+  //                                         } else{
+  //                                           return const GFLoader(type: GFLoaderType.ios,);
+  //                                         }
+  //                                       }
+  //                                     ),
+  //                                   )
+  //                                 ],
+  //                               ),
+  //                             ),
+  //                           );
+  //                       }
+  //                     ),
+  //                   );
+  //                 },
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //       );
+  // }
+
+
 }
